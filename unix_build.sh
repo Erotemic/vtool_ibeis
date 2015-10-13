b1fa17d1d5537c5bd19dad2500b856db3ce85750
joncrall <erotemic@gmail.com>
2015-10-13 10:46:36 -0400

    wip

--- unix_build.sh
+++ unix_build.sh
@@ -2,6 +2,8 @@
 #cd ~/code/vtool
 echo "[sver.unix_build] checking if build dir should be removed"
 
+export FAILCMD='{ echo "FAILED VTOOL BUILD" ; exit 1; }'
+
 python2.7 -c "import utool as ut; print('keeping build dir' if ut.get_argflag('--no-rmbuild') else ut.delete('build'))" $@
 
 mkdir build
@@ -9,22 +11,30 @@ cd build
 
 echo "$OSTYPE"
 
+if [[ "$VIRTUAL_ENV" == ""  ]]; then
+    export LOCAL_PREFIX=/usr/local
+    export _SUDO="sudo"
+else
+    export LOCAL_PREFIX=$($PYEXE -c "import sys; print(sys.prefix)")/local
+    export _SUDO=""
+fi
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -G "Unix Makefiles" ..  || { echo "FAILED CMAKE CONFIGURE" ; exit 1; }
+    cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LOCAL_PREFIX -DOpenCV_DIR=$LOCAL_PREFIX/share/OpenCV ..  || $FAILCMD
 elif [[ "$OSTYPE" == "msys"* ]]; then
     echo "USE MINGW BUILD INSTEAD" ; exit 1
     export INSTALL32="c:/Program Files (x86)"
     echo "INSTALL32=$INSTALL32"
-    cmake -G "MSYS Makefiles" -DOpenCV_DIR="$INSTALL32/OpenCV" .. || { echo "FAILED CMAKE CONFIGURE" ; exit 1; }
+    cmake -G "MSYS Makefiles" -DOpenCV_DIR="$INSTALL32/OpenCV" .. || $FAILCMD
 else
-    cmake -G "Unix Makefiles" ..  || { echo "FAILED CMAKE CONFIGURE" ; exit 1; }
+    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LOCAL_PREFIX -DOpenCV_DIR=$LOCAL_PREFIX/share/OpenCV ..  ||  $FAILCMD
 fi
 
 if [[ "$OSTYPE" == "msys"* ]]; then
-    make || { echo "FAILED MAKE" ; exit 1; }
+    make ||  $FAILCMD
 else
     export NCPUS=$(grep -c ^processor /proc/cpuinfo)
-    make -j$NCPUS || { echo "FAILED MAKE" ; exit 1; }
+    make -j$NCPUS ||  $FAILCMD
 fi
 
 cp -v libsver* ../vtool
