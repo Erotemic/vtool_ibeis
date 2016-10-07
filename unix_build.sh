58bb89f7773d137a77e8ce31a2eb4ad7210c307a
Jason Parham <bluemellophone@gmail.com>
2016-10-07 12:57:21 -0400

    Small change for OMP updates

--- unix_build.sh
+++ unix_build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-#cd ~/code/vtool
+
 echo "[sver.unix_build] checking if build dir should be removed"
-
 export FAILCMD='{ echo "FAILED VTOOL BUILD" ; exit 1; }'
-
 python2.7 -c "import utool as ut; print('keeping build dir' if ut.get_argflag('--no-rmbuild') else ut.delete('build'))" $@
 
+#################################
+echo 'Removing old build'
+rm -rf build
+rm -rf CMakeFiles
+rm -rf CMakeCache.txt
+rm -rf cmake_install.cmake
+#################################
+echo 'Creating new build'
 mkdir build
 cd build
-
-echo "$OSTYPE"
+#################################
 
 export PYEXE=$(which python2.7)
 if [[ "$VIRTUAL_ENV" == ""  ]]; then
@@ -20,12 +25,10 @@ else
     export _SUDO=""
 fi
 
-echo "LOCAL_PREFIX = $LOCAL_PREFIX"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LOCAL_PREFIX -DOpenCV_DIR=$LOCAL_PREFIX/share/OpenCV ..  || $FAILCMD
-    echo 'Fixing OSX libiomp'
-    install_name_tool -change libiomp5.dylib ~/code/libomp_oss/exports/mac_32e/lib.thin/libiomp5.dylib lib*
+echo 'Configuring with cmake'
+if [[ '$OSTYPE' == 'darwin'* ]]; then
+    export CONFIG="-DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_C_COMPILER=clang2 -DCMAKE_CXX_COMPILER=clang2++ -DCMAKE_INSTALL_PREFIX=$LOCAL_PREFIX -DOpenCV_DIR=$LOCAL_PREFIX/share/OpenCV"
+    cmake $CONFIG -G 'Unix Makefiles' ..
 elif [[ "$OSTYPE" == "msys"* ]]; then
     echo "USE MINGW BUILD INSTEAD" ; exit 1
     export INSTALL32="c:/Program Files (x86)"
