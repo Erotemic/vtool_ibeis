e09947fe479a24887ad7c3adbe6bd51e92bcbf49
joncrall <erotemic@gmail.com>
2019-12-10 16:05:24 -0500

    wip

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # Install dependency packages
+
+#if [[ "$(which docker)" != "" ]]; then
+#    echo "building with docker"
+#    BDIST_FPATH=$(./run_multibuild.sh)
+#    pip install $BDIST_FPATH
+#else
+
 pip install -r requirements.txt
 # new pep makes this not always work
 # pip install -e .
@@ -7,9 +14,9 @@ pip install -r requirements.txt
 python setup.py build_ext --inplace
 python setup.py develop
 
-
 # Point to custom opencv
 _note="""
 python setup.py build_ext --inplace -- -DOpenCV_DIR=$HOME/code/opencv/build
 python setup.py develop -- -DOpenCV_DIR=$HOME/code/opencv/build
 """
+#fi
