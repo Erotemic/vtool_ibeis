50edcd15ddf7ca88930a0a98c2811177e4a027a4
Michael Mulich <michael.mulich@gmail.com>
2020-06-24 10:04:54 -0700

    switch to macports

--- scripts/_install_opencv_build_dependencies_on_macos.sh
+++ scripts/_install_opencv_build_dependencies_on_macos.sh
@@ -4,10 +4,28 @@
 
 set -ex
 
-brew install pkg-config
-brew install jpeg libpng libtiff openexr
-brew install eigen tbb
-#: to acquire the source
-brew install git
+sudo port install \
+     clang-5.0 \
+     clang_select \
+     eigen3-devel \
+     freetype \
+     gdal \
+     google-glog \
+     hdf5 \
+     harfbuzz \
+     lapack \
+     leptonica \
+     libomp \
+     llvm-5.0 \
+     llvm_select \
+     OpenBLAS-devel \
+     py36-h5py-devel \
+     py36-gdal \
+     tesseract \
+
+# OpenMP
+sudo port select clang mp-clang-5.0
+sudo port select llvm mp-llvm-5.0
+
 #: needed for python3 bindings
 python -m pip install numpy
