ccdd81f12047747ac0b0defd1835ad14ea4a596e
Jason Parham <bluemellophone@gmail.com>
2020-07-02 15:53:49 -0700

    Try to slim down the macOS wheel

--- scripts/_install_opencv_build_dependencies_macports_on_macos.sh
+++ scripts/_install_opencv_build_dependencies_macports_on_macos.sh
@@ -8,36 +8,12 @@ sudo port selfupdate
 
 # Install ports if MacPorts install location is not present
 sudo port install \
-     clang-5.0 \
-     clang_select \
-     llvm-5.0 \
-     llvm_select \
-     libomp \
      gflags \
-     libpng \
-     jasper \
-     ilmbase \
-     openexr \
-     webp \
-     jpeg \
-     tiff \
-     tbb \
-     libdc1394 \
      eigen3-devel \
-     boost \
-     bzip2 \
-     zlib \
-     ffmpeg \
-     freeglut \
-     mesa \
-     gstreamer1-gst-plugins-base \
-     qt5-qtbase \
      gtk2 \
-     ade
-
-# OpenMP
-sudo port select clang mp-clang-5.0
-sudo port select llvm mp-llvm-5.0
+     libdc1394 \
+     ffmpeg \
+     gstreamer1-gst-plugins-base
 
 #: needed for python3 bindings
 python -m pip install numpy
--- scripts/_install_opencv_build_software_on_macos.sh
+++ scripts/_install_opencv_build_software_on_macos.sh
@@ -10,6 +10,7 @@ fi
 
 export OPENCV_VERSION=4.2.0
 export WORKSPACE=$PWD
+export VIRTUAL_ENV="/opt/opencv"
 
 # Checkout source, only if it isn't already laying around
 if [ ! -d $WORKSPACE/opencv ]; then
@@ -24,21 +25,24 @@ fi
 # Build OpenCV, only if it's not already installed
 if [ ! -d /opt/opencv ]; then
     # Create a new build space
+    rm -rf $WORKSPACE/opencv/build
     mkdir -p $WORKSPACE/opencv/build
     cd $WORKSPACE/opencv/build
     # -D CMAKE_C_COMPILER=clang \
     # -D CMAKE_CXX_COMPILER=clang \
     cmake \
         -D CMAKE_BUILD_TYPE=RELEASE \
-        -D CMAKE_INSTALL_PREFIX=/opt/opencv/ \
+        -D CMAKE_INSTALL_PREFIX=${VIRTUAL_ENV} \
         -D OPENCV_GENERATE_PKGCONFIG=ON \
         -D ENABLE_PRECOMPILED_HEADERS=OFF \
-        -D BUILD_opencv_apps=OFF \
         -D BUILD_SHARED_LIBS=OFF \
         -D BUILD_TESTS=OFF \
         -D BUILD_PERF_TESTS=OFF \
         -D BUILD_DOCS=OFF \
         -D BUILD_EXAMPLES=OFF \
+        -D BUILD_opencv_apps=OFF \
+        -D BIILD_opencv_freetype=OFF \
+        -D BUILD_opencv_hdf=OFF \
         -D BUILD_opencv_java=OFF \
         -D BUILD_opencv_python2=OFF \
         -D BUILD_opencv_python3=ON \
@@ -46,24 +50,29 @@ if [ ! -d /opt/opencv ]; then
         -D INSTALL_C_EXAMPLES=OFF \
         -D INSTALL_PYTHON_EXAMPLES=OFF \
         -D INSTALL_CREATE_DISTRIB=ON \
+        -D BUILD_ZLIB=ON \
         -D BUILD_JPEG=ON \
+        -D BUILD_WEBP=ON \
+        -D BUILD_PNG=ON \
         -D BUILD_TIFF=ON \
-        -D BUILD_HDR=ON \
+        -D BUILD_JASPER=ON \
+        -D BUILD_OPENEXR=ON \
         -D WITH_MATLAB=OFF \
-        -D WITH_TBB=ON \
+        -D WITH_TBB=OFF \
         -D WITH_CUDA=OFF \
         -D WITH_CUBLAS=0 \
         -D WITH_EIGEN=ON \
         -D WITH_AVFOUNDATION=ON \
-        -D WITH_JPEG=ON \
         -D WITH_HDR=ON \
-        -D WITH_V4L=ON \
-        -D WITH_GDAL=ON \
+        -D WITH_GDAL=OFF \
         -D WITH_WIN32UI=OFF \
         -D WITH_QT=OFF \
-        -D PYTHON_LIBRARY=`python -c 'import subprocess ; import sys ; s = subprocess.check_output("python-config --configdir", shell=True).decode("utf-8").strip() ; (M, m) = sys.version_info[:2] ; print("{}/libpython{}.{}.dylib".format(s, M, m))'` \
-        -D PYTHON_INCLUDE_DIR=`python -c 'import distutils.sysconfig as s; print(s.get_python_inc())'` \
-        -D PYTHON_EXECUTABLE=`which python3.6` \
+        -D PYTHON3_EXECUTABLE=$(which python3) \
+        -D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+        -D PYTHON3_INCLUDE_DIR2=$(python3 -c "from os.path import dirname; from distutils.sysconfig import get_config_h_filename; print(dirname(get_config_h_filename()))") \
+        -D PYTHON3_LIBRARY=$(python3 -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))") \
+        -D PYTHON3_NUMPY_INCLUDE_DIRS=$(python3 -c "import numpy; print(numpy.get_include())") \
+        -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
         -D CMAKE_C_FLAGS="${CMAKE_C_FLAGS} -I/opt/local/include -stdlib=libc++" \
         -D CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -I/opt/local/include -stdlib=libc++" \
         -D CMAKE_SHARED_LINKER_FLAGS="${CMAKE_SHARED_LINKER_FLAGS} -L/opt/local/lib -lc++" \
