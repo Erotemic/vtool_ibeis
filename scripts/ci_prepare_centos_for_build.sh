c48e367529b4b3d34e2ae7395ae624a7102829d6
Jason Parham <bluemellophone@gmail.com>
2021-12-02 01:58:52 -0800

    Debug yum install, disable fast fail

--- scripts/ci_prepare_centos_for_build.sh
+++ scripts/ci_prepare_centos_for_build.sh
@@ -5,4 +5,6 @@ set -ex
 # Install Python package dependencies
 python -m pip install -r requirements/build.txt
 
+yum update
+
 yum install -y eigen3 gflags-devel opencv-devel libgomp-devel pkgconfig
--- scripts/ci_prepare_macos_for_build.sh
+++ scripts/ci_prepare_macos_for_build.sh
@@ -8,4 +8,6 @@ export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 # Install Python package dependencies
 python -m pip install -r requirements/build.txt
 
+brew update
+
 brew install -y eigen gflags opencv libomp pkg-config
