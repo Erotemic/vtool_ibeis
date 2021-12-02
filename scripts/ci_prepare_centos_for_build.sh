ac64b24900408274ee36f3ede1860615013a0908
Jason Parham <bluemellophone@gmail.com>
2021-12-02 03:27:18 -0800

    Updates for building

--- scripts/ci_prepare_centos_for_build.sh
+++ scripts/ci_prepare_centos_for_build.sh
@@ -2,9 +2,8 @@
 
 set -ex
 
-# Install Python package dependencies
-python -m pip install -r requirements/build.txt
-
-yum update
-
-yum install -y eigen3 opencv opencv-devel opencv-python libgomp-devel pkgconfig
+apt-get install -y \
+	pkg-config \
+	libeigen3-dev \
+	libopencv-dev \
+	libomp-dev
--- scripts/ci_prepare_macos_for_build.sh
+++ scripts/ci_prepare_macos_for_build.sh
@@ -5,9 +5,8 @@ set -ex
 # See https://stackoverflow.com/a/246128/176882
 export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Install Python package dependencies
-python -m pip install -r requirements/build.txt
-
-brew update
-
-brew install eigen opencv libomp pkg-config
+brew install \
+	pkg-config \
+	eigen \
+	opencv \
+	libomp
