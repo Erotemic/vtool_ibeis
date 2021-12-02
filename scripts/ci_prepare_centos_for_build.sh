918d13062e02c60976206ecbf0f25db13c4c9c85
Jason Parham <bluemellophone@gmail.com>
2021-12-02 01:54:24 -0800

    Update build

--- scripts/ci_prepare_centos_for_build.sh
+++ scripts/ci_prepare_centos_for_build.sh
@@ -2,4 +2,7 @@
 
 set -ex
 
-yum install -y gflags-devel
+# Install Python package dependencies
+python -m pip install -r requirements/build.txt
+
+yum install -y eigen3 gflags-devel opencv-devel libgomp-devel pkgconfig
--- scripts/ci_prepare_macos_for_build.sh
+++ scripts/ci_prepare_macos_for_build.sh
@@ -8,6 +8,4 @@ export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 # Install Python package dependencies
 python -m pip install -r requirements/build.txt
 
-$CUR_LOC/_install_opencv_build_dependencies_macports_on_macos.sh
-
-$CUR_LOC/_install_opencv_build_software_on_macos.sh
+brew install -y eigen gflags opencv libomp pkg-config
