c5ae659434fa2068a820fd4e9c47866682e6002b
tsubramanian <tamilselvan@gmail.com>
2023-05-22 21:26:44 +0530

    Update trig.py

--- vtool/trig.py
+++ vtool/trig.py
@@ -16,9 +16,11 @@ def atan2(y, x):
         >>> y = rng.rand(1000).astype(np.float64)
         >>> x = rng.rand(1000).astype(np.float64)
         >>> theta = atan2(y, x)
+        >>> print("theta:::",theta)        
         >>> assert np.all(theta >= 0)
         >>> assert np.all(theta < 2 * np.pi)
         >>> import ubelt as ub
+        >>> print("theta hashdata:::",ub.hash_data(theta))        
         >>> assert ub.hash_data(theta) in [
         >>>     '6bfc86a2e94dd2dafbf501035719a7873d57f5f8e9cde88c4ccc35e98bb9e7b82abf6230803a923be7060866d66b8ac567388803593f9b7c763163a24282442a',
         >>>     '90fe55311562f1c3ae451d5c4f27573259fed96752a5bd03f0f1216b46cf5b4b48024dcc744bfc6df7e6f8d6eb2a2a4b31b9f5ca75c0064b37acd09303811d76',
